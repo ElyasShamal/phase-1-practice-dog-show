2d5a162a7d4f42e6dab51e3977c6ee61e3ea6255
shamalelyas <shamalm3398@connect.durhamtch.edu>
2024-06-24 13:31:54 -0400

    fetch data and create render function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("http://localhost:3000/dogs")
+    .then((responce) => responce.json())
+    .then((dogs) => dogs.forEach(renderDogs));
+});
 
-})
+function renderDogs(dogs) {
+  let tabelBody = document.getElementById("table-body");
+  let tableData = `<tr>
+  <td>${dogs.name}</td>
+  <td>${dogs.breed}</td>
+  <td>${dogs.sex}</td>
+  <td><button class="edit-btn">Edit</button></td>
+</tr>`;
+
+  tabelBody.innerHTML += tableData;
+}
